bfbab1d83e1cf3a6d863c55b8a0f494085e04dd4
SergentKarote <Pouradier.clement@gmail.com>
2023-09-26 14:51:30 +0200

    Config Database

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  'cloud_82z9', // Database name
+  'clouduser', // User
+  'RvdlQUEVL5I27OdYqRU4puyHV3Q3ajtR', // MDP
   {
-    host: '', // Host
+    host: 'dpg-ck9d0f9ehpqs73c3hfp0-a', // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
